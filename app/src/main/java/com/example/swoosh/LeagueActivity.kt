42db59ab0e3b6dd48800c9bf896861e96137b97d
Jack <jackcurtin94@gmail.com>
2021-11-18 09:33:36 -0600

    encounters and resolved error about var init on league activity - added lateinit to vars

--- app/src/main/java/com/example/swoosh/LeagueActivity.kt
+++ app/src/main/java/com/example/swoosh/LeagueActivity.kt
@@ -1,14 +1,46 @@
 package com.example.swoosh
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.ToggleButton
 
 class LeagueActivity : BaseActivity() {
+
+    var selectedLeague = ""
+    lateinit var mensBtn:ToggleButton
+    lateinit var womensBtn:ToggleButton
+    lateinit var coedBtn:ToggleButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
+        mensBtn = findViewById(R.id.mensLeagueBtn)
+        womensBtn = findViewById(R.id.womensLeagueBtn)
+        coedBtn = findViewById(R.id.coedLeagueBtn)
+    }
+
+    fun onMensClicked(view: View) {
+        womensBtn.isChecked = false
+        coedBtn.isChecked = false
+
+        selectedLeague = "mens"
+    }
+
+    fun onWomensClicked(view:View) {
+        mensBtn.isChecked = false
+        coedBtn.isChecked = false
+
+        selectedLeague = "womens"
+    }
+
+    fun onCoedClicked(view:View) {
+        mensBtn.isChecked = false
+        womensBtn.isChecked = false
+
+        selectedLeague = "coed"
     }
 
     fun leagueNextClicked(view: View) {
--- app/src/main/java/com/example/swoosh/SkillActivity.kt
+++ app/src/main/java/com/example/swoosh/SkillActivity.kt
@@ -3,7 +3,7 @@ package com.example.swoosh
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class SkillActivity : AppCompatActivity() {
+class SkillActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_skill)
